6e12b0719c3bf7b8fb0353337cb158efbbd5b4f2
orionjohnston <80126030+orionjohnston@users.noreply.github.com>
2025-03-07 19:21:53 -0500

    Delete js/script.js

--- js/script.js
+++
@@ -1,9 +0,0 @@
-// Sticky Header Shadow
-window.addEventListener('scroll', function() {
-    var header = document.querySelector('header');
-    if (window.pageYOffset > 50) {
-        header.classList.add('scrolled');
-    } else {
-        header.classList.remove('scrolled');
-    }
-});
